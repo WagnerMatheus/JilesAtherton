286649c9973611f35df92a372cc62268e47dc832
Matheus <matheuswagner01@gmail.com>
2025-05-21 08:34:46 -0300

    B as input

--- scalar.m
+++ scalar.m
@@ -20,6 +20,12 @@
 % (Bergqvist) [1], algumas questões numéricas fazem com que a formulação
 % baseada em `M_ir` seja cerca de 500x mais eficiente.
 %
+%   Além disso, levando em consideração que em modelos de elementos finitos a
+% variável de estado é comumente o potencial (vetor) magnético, de onde se
+% encontra de forma direta a indução magnética, a formulação aqui utilizada
+% toma como excitação a indução, diferentemente do modelo original, que é
+% excitado por campo magnético.
+%
 %   OBS.: este script foi testado em Octave e pode necessitar pequenas
 % adaptações de sintaxe para rodar em MATLAB. Uma alternativa é utilizar o
 % `OctaveOnline`: https://octave-online.net/
@@ -29,11 +35,28 @@
 %   Neste script a formulação com `M` sendo a variável de estado é utilizada.
 % Ou seja, o problema a ser resolvido é
 %
-%     dM   dM dH
-%     -- = -- -- ,  (1)
-%     dt   dH dt
+%                          - t
+%                        /
+%                       |  dM
+%     M(t) = M(ti) +    |  -- dt ,  (1)
+%                       |  dt
+%                      /
+%                    -  ti
 %
-%  onde
+% sendo `ti` o tempo inicial, e utiliza-se a indução magnética (B) como
+% excitação
+%
+%     dM   dM dB
+%     -- = -- -- ,  (2)
+%     dt   dB dt
+%
+%               dM
+%               --
+%     dM        dH
+%     -- = -------------- ,
+%     dB             dM
+%          mu0 ( 1 + -- )
+%                    dH
 %
 %                   dM_an         dM_ir
 %                 c ----- + (1-c) -----
@@ -44,29 +67,40 @@
 %                        dHe            dHe
 %
 % sendo `alpha` (acoplamento inter-domínio) e `c` (reversibilidade da
-% magnetização) parâmetros do modelo, `H` o campo magnético e (baseando-se em
-% Bergqvist [1] para a derivada de `M_ir`)
+% magnetização) parâmetros do modelo, `mu0` a permeabilidade magnética do
+% vácuo, `H` o campo magnético e (baseando-se em Bergqvist [1] para a
+% derivada de `M_ir`)
 %
 %     He = H + alpha M,
 %
 %     M_an = ms (coth(He/a)-(a/He)),
 %
-%                 - | M_an - M_ir |                     dH
-%     dM_ir     /    --------------- , se (M_an - M_ir) -- > 0
+%                 -  | M_an - M_ir |                    dH
+%     dM_ir     /    --------------- , se (M_an - M_ir) -- > 0     (3)
 %     ----- = -             k                           dt      ,
 %      dHe      \
-%                  -        0        , caso contrário
+%                 -         0        , caso contrário
 %
-% onde `ms` (magnetização de saturação), `a` (densidade de paredes de domínio)
+% sendo `ms` (magnetização de saturação), `a` (densidade de paredes de domínio)
 % e `k` (energia média necessária para romper `fixamento` (pinning)) são
 % parâmetros do modelo e
 %
-%           M - c M_an
-%     M_ir = ---------- .  (2)
+%            M - c M_an
+%     M_ir = ---------- .  (4)
 %              1-c
 %
-%   Na equação 2 fica implícita a relação entre as componente reversível (M_an)
-% e irreversível (M_ir) da magnetização total (M):
+%   Neste trabalho, `dH/dt` na equação 3 será substituído por `dB/dt`, visto
+% que a derivada de ambos no caso escalar tem o mesmo sinal, assim
+%
+%                 -  | M_an - M_ir |                    dB
+%     dM_ir     /    --------------- , se (M_an - M_ir) -- > 0
+%     ----- = -             k                           dt      .
+%      dHe      \
+%                 -         0        , caso contrário
+%
+%   Também nota-se que na equação 4 fica implícita a relação entre as
+% componentes reversível (M_an) e irreversível (M_ir) da magnetização total
+% (M):
 %
 %     M = c M_an + (1-c) M_ir.
 %
@@ -77,7 +111,9 @@
 %
 %   Esse tópico é tema de pesquisa há várias décadas, pois é fundamental para
 % muitas áreas da física e engenharia. Na elétrica, qualquer estudo transiente
-% de equipamento ou circuito precisa resolver numericamente um PVI.
+% de equipamento ou circuito precisa resolver numericamente um PVI. Dessa
+% forma, existem diversos algoritmos exaustivamente estudados e testados à
+% nossa disposição.
 %
 %   Para este script em questão, é relevante saber que os algoritmos
 % implementados para resolução de PVIs ditos explícitos (caso do modelo de
@@ -94,13 +130,14 @@
 %   3. IMPLEMENTAÇÃO
 %
 %   A implementação consiste em fornecer uma função `dmdt` que calcule o lado
-% esquerdo da equação 1, tendo como entradas o valor atual de `M` e a excitação
-% `H` e `dH/dt`, ou seja,
+% esquerdo da equação 2, tendo como entradas o valor atual de `M` e a excitação
+% `B` e `dB/dt`, ou seja,
 %
-%     f(M, t) = dmdt(M, H(t), dHdt(t)).
+%     f(M, t) = dmdt(M, B(t), dBdt(t)).
 %
-%   Assim, também se faz necessário fornecer uma excitação de campo magnético
-% arbitrária, juntamente com uma função para cálculo da sua derivada.
+%   Assim, também se faz necessário fornecer uma excitação de indução
+%   magnética arbitrária, juntamente com uma função para cálculo da sua
+%   derivada.
 %
 %   3.1. INTEGRADORES DE EDO DO OCTAVE/MATLAB
 %
@@ -146,8 +183,8 @@
 %   Alguns gráficos são gerados para visualização dos resultados, são eles:
 %
 %  - Laço de histerese;
-%  - Indução no tempo;
 %  - Campo magnético no tempo;
+%  - Indução no tempo;
 %  - Componentes da magnetização no tempo.
 %
 %   5. REFERÊNCIAS
@@ -164,7 +201,7 @@
 clear all
 
 %%%%% CONSTANTES FÍSICAS %%%%%
-%mu0 = 4e-7*pi;
+global mu0 = 4e-7*pi;
 
 %%%%% PARÂMETROS DO MATERIAL %%%%%
 global ms = 1145500; global a = 59; global k = 99; global c = .55;
@@ -178,7 +215,6 @@ global slope_iso_lc = ms * (1.0/tanh(man_iso_lc)-(1/man_iso_lc))/(a*man_iso_lc);
 
 %%%%% DEFINIÇÃO DAS FUNÇÕES DO MODELO %%%%%
 % M_an
-% magnetização anisterética isotrópica
 function result = man_iso(He)
   global a; global man_iso_lc; global ms; global slope_iso_lc;
   if abs(He/a) > man_iso_lc
@@ -208,10 +244,10 @@ end
 % -----
 %  dHe
 % definição baseada em Bergqvist [1]
-function result = dmirdHe(man, mi, dHdt)
+function result = dmirdHe(man, mi, dBdt)
   global k;
   aux = (man - mi)/k;
-  if dHdt * aux > 0
+  if dBdt * aux > 0
     result = abs(aux);
   else
     result = 0;
@@ -221,39 +257,43 @@ end
 % dM
 % --
 % dt
-function result = dmdt(M, H, dHdt)
-  global alpha; global c;
+function result = dmdt(M, B, dBdt)
+  global mu0; global alpha; global c;
+  H = B/mu0 - M;
   He = H + alpha * M;
   M_an = man_iso(He);
   M_ir = (M - c*M_an) / (1-c);
   dM_andHe = dmandHe(He);
-  dM_irdHe = dmirdHe(M_an, M_ir, dHdt);
+  dM_irdHe = dmirdHe(M_an, M_ir, dBdt);
   dMdH = (c*dM_andHe + (1-c)*dM_irdHe) ...
           / (1 - alpha * (c*dM_andHe + (1-c)*dM_irdHe));
-  result = dMdH * dHdt;
+  dMdB = dMdH / (mu0 * (1 + dMdH));
+  result = dMdB * dBdt;
 end
 
 %%%%% DEFINIÇÃO DAS FUNÇÕES DE EXCITAÇÃO %%%%%
 
-% amplitude de campo magnético
-global H_amp = 400;
+% amplitude de indução magnétic
+global B_amp = 1.3;
 
-% frequência do campo magnético
-global H_freq = 1;
+% frequência da indução magnética
+global B_freq = 1;
 
-% H(t)
-function result = H(t)
-  global H_amp; global H_freq;
-  result = t .* (H_amp*sin(2*pi*H_freq*t));
+% B(t)
+function result = B(t)
+  global B_amp; global B_freq;
+  %result = t .* (B_amp*sin(2*pi*B_freq*t));
+  result = (B_amp*sin(2*pi*B_freq*t));
 end
 
-% dH
+% dB
 % --
 % dt
-function result = dHdt(t)
-  global H_amp; global H_freq;
-  result = H_amp*sin(2*pi*H_freq*t) ...
-           + t .* (2*pi*H_freq*H_amp*cos(2*pi*H_freq*t));
+function result = dBdt(t)
+  global B_amp; global B_freq;
+  %result = B_amp*sin(2*pi*B_freq*t) ...
+  %         + t .* (2*pi*B_freq*B_amp*cos(2*pi*B_freq*t));
+  result = (2*pi*B_freq*B_amp*cos(2*pi*B_freq*t));
 end
 
 %%%%% DEFINIÇÃO DOS PARÂMETROS E RESOLUÇÃO %%%%%
@@ -261,8 +301,8 @@ end
 % intervalo de simulação
 ti = 0; tf = 1;
 
-% magnetização inicial
-M_0 = 0;
+% magnetização inicial (tal que H(ti) = 0)
+M_0 = B(ti)/mu0;
 
 % número de passos no tempo para os quais se tem interesse na solução
 num_steps = 100;
@@ -273,7 +313,7 @@ ode_options = odeset(
   'MaxStep', 1e-1,
   'InitialStep', 1e-3);
 
-f = @(M, t) dmdt(M, H(t), dHdt(t));
+f = @(M, t) dmdt(M, B(t), dBdt(t));
 
 %%%%% SOLUÇÃO UTILIZANDO O LSODE %%%%%
 
